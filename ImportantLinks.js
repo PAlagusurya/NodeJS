7b2376a0da64a80c5673dba0127cb682af79dbb8
PAlagusurya <alagusuryap@gmail.com>
2025-01-03 20:45:34 +0530

    Garbage collectors

---
+++ ImportantLinks.js
@@ -0,0 +1,16 @@
+/*
+ *
+ *
+ * Garbage Collection in V8
+ *
+ * V8 manages memory efficiently using multiple garbage collectors (Oilpan, Orinoco, Scavenger, Mark-Compact).
+ * Each collector is specialized to optimize performance for specific tasks.
+ * They use mark-and-sweep algorithms for efficient memory management.
+ */
+
+/**
+ *  JIT: https://v8.dev/blog/launching-ignition-and-turbofan
+ *
+ *
+ *
+ */
